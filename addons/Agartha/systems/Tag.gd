682337c6e0654698dfe0f63387b1c593dabb0fb5
vaasref <me@vaasref.io>
2021-03-12 07:29:35 +0100

    Making tags case insensitive.

--- addons/Agartha/systems/Tag.gd
+++ addons/Agartha/systems/Tag.gd
@@ -4,15 +4,15 @@ extends Node
 
 func get(tag:String, strict:bool=false):
 	var output = []
-	tag = "# " + tag.trim_prefix("# ")
+	tag = "# " + tag.trim_prefix("# ").to_lower()
 	if strict:
 		output.get_tree().get_nodes_in_group(tag)
 	else:
 		for n in get_tree().get_nodes_in_group("tagged"):
 			for g in n.get_groups():
-				var t = g.replace("$", n.get_name())
-				if g.begin_with(t):
-					output += n
+				var t = g.replace("$", n.get_name()).to_lower()
+				if t.begins_with(tag):
+					output.append(n)
 					break
 	if output.size() == 1:
 		output = output[0]
@@ -20,22 +20,22 @@ func get(tag:String, strict:bool=false):
 
 func get_tags(tag:String):
 	var output = []
-	tag = "# " + tag.trim_prefix("# ")
+	tag = "# " + tag.trim_prefix("# ").to_lower()
 	output.append(tag)
 	for n in get_tree().get_nodes_in_group("tagged"):
 		for g in n.get_groups():
-			var t = g.replace("$", n.get_name())
-			if g.begin_with(t):
-				output += g
+			var t = g.replace("$", n.get_name()).to_lower()
+			if t.begins_with(tag):
+				output += t
 	return output
 
 func get_node_dict(tag:String):
 	var output = {}
-	tag = "# " + tag.trim_prefix("# ")
+	tag = "# " + tag.trim_prefix("# ").to_lower()
 	for n in get_tree().get_nodes_in_group("tagged"):
 		for g in n.get_groups():
-			var t = g.replace("$", n.get_name())
-			if g.begin_with(t):
+			var t = g.replace("$", n.get_name()).to_lower()
+			if t.begins_with(tag):
 				if t in output:
 					output[t].append(n)
 				else:
